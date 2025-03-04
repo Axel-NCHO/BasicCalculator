ebd0727b66106665ec4cec43faf9e1f38a6a746f
Axel-NCHO <naxelamour09+github@gmail.com>
2025-03-04 11:11:45 +0100

    Fix number detection and make the program interactive

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+
 import argparse
 import os
 
+###############################################################################
+DEBUG = False
 
+
+###############################################################################
 class BinaryTree:
+    """Binary tree implementation"""
     def __init__(self, value):
         self.val = value
         self.left = None
@@ -14,15 +23,20 @@ class BinaryTree:
     def set_right(self, node):
         self.right = node
 
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
     def __repr__(self):
         return f"({self.val} {self.left} {self.right})" if self.left and self.right else str(self.val)
 
 
+###############################################################################
 class ExpressionParser:
+    """"""
     PRECEDENCE = {'+': 1, '-': 1, '*': 2, '/': 2}
 
-    def __init__(self, tokens):
-        self.tokens = tokens
+    def __init__(self, expr: str):
+        self.tokens = list(expr.replace(' ', ''))
         self.index = 0
 
     def __current_token(self):
@@ -38,6 +52,11 @@ class ExpressionParser:
 
         if token.isnumeric():  # Direct number
             self.__advance()
+            next_token = self.__current_token()
+            while next_token and next_token.isnumeric():
+                token += next_token
+                self.__advance()
+                next_token = self.__current_token()
             return BinaryTree(token)
 
         elif token == "(":  # Handle parentheses
@@ -91,6 +110,27 @@ class ExpressionParser:
         return operand_stack[0]  # Root of the expression tree
 
 
+###############################################################################
+def evaluate_tree(eval_tree: BinaryTree):
+    if eval_tree.is_leaf():
+        val: str = eval_tree.val
+        return float(val) if '.' in val else int(val)
+    eval_left: int|float = evaluate_tree(eval_tree.left)
+    eval_right: int|float = evaluate_tree(eval_tree.right)
+    match eval_tree.val:
+        case "+":
+            return eval_left + eval_right
+        case "-":
+            return eval_left - eval_right
+        case "*":
+            return eval_left * eval_right
+        case "/":
+            return eval_left / eval_right
+        case _:
+            raise ValueError(f"Unexpected operator: {eval_tree.val}")
+
+
+###############################################################################
 def get_args():
 
     """
@@ -98,18 +138,35 @@ def get_args():
     """
     script_name = os.path.basename(__file__)
     my_parser = argparse.ArgumentParser(description="Evaluate a basic math expression with number, parentheses and binary operators +, -, *, and /.")
-    my_parser.add_argument("-e", "--expression", type=str, required=True,
+    my_parser.add_argument("-d", "--debug", default=False, action="store_true",
                            help="The expression to evaluate. Should only contain numbers, parentheses and binary operators +, -, *, and /.")
     return my_parser.parse_args()
 
 
+###############################################################################
 if __name__ == "__main__":
-    expression: str = get_args().expression
-    if not expression:
-        raise ValueError("No expression provided")
-    expression = expression.replace(" ", "")
-    tokens = list(expression)
-    print(tokens)
-    parser = ExpressionParser(tokens)
-    tree = parser.operands_list()
-    print(tree)
+    args = get_args()
+    DEBUG = args.debug
+    print("BasicCalculator.\nThis program evaluate simple math expression that only contain numbers, parentheses and binary operators +, -, *, and /.")
+    print("You can either enter an expression or 'q' or 'Q' to exit the program.\n")
+    if DEBUG:
+        print("Running with debug mode.")
+    expression = str(input("Enter an expression $ "))
+    while expression.lower() != "q":
+        try:
+            if DEBUG:
+                print(f"Evaluating expression {expression}")
+            parser = ExpressionParser(expression)
+            tree = parser.operands_list()
+            if DEBUG:
+                print(f"Evaluation tree: {tree}")
+            if DEBUG:
+                print(f"Evaluation:", end=" ")
+            print(evaluate_tree(tree))
+        except ValueError as e:
+            print(e)
+        finally:
+            print()
+            expression = str(input("Enter an expression $ "))
+
+    print("\nGoodbye!")
